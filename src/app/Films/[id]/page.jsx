a484d6eb09efb467928024d2c20fb75e4a32af4b
Agustin <agustinperea5@gmail.com>
2024-05-09 19:01:44 -0300

    add(-):Completado pagina Films

---
+++ src/app/Films/[id]/page.jsx
@@ -0,0 +1,7 @@
+export default async function FilmSpecific ({ params }) {
+  return (
+    <section>
+      <h1>Hola </h1>
+    </section>
+  )
+}
---
+++ src/app/Personajes/[id]/page.jsx
@@ -0,0 +1,7 @@
+export default function Personajes () {
+  return (
+    <div>
+      <h1>Personaje numero</h1>
+    </div>
+  )
+}
