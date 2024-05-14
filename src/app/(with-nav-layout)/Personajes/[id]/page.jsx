e1e9fae4f37b98fe8a483e55f4b4b59dbcf91be9
Agustin <agustinperea5@gmail.com>
2024-05-14 16:38:34 -0300

    Add(-):Creado componente para personaje especifico

--- src/app/(with-nav-layout)/Personajes/[id]/page.jsx
+++ src/app/(with-nav-layout)/Personajes/[id]/page.jsx
@@ -1,7 +1,19 @@
-export default function Personajes () {
+// En este componente se encontrará el componente principal de la ruta para Personaje especifica. Aquí se estará llamando a la API
+import CharSpecific from './CharSpecific'
+
+const fetchOneChar = (id) => {
+  return fetch(`https://swapi.dev/api/people/${id}`)
+    .then(res => res.json())
+}
+
+export default async function PersonajesSpecificPage ({ params }) {
+  const { id } = params // Algo que me agarró desprevenido es que el id de cada elemento de la api no corresponde al episode_id, sino que al index+1
+  const char = await fetchOneChar(id)
+
   return (
-    <div>
-      <h1>Personaje numero</h1>
-    </div>
+    <section className='h-[800px] flex flex-col justify-center'>
+
+      <CharSpecific char={char} />
+    </section>
   )
 }
