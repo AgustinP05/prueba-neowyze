d56e0c41ebcd0b10408f7aba7b62af81468eacf5
Agustin <agustinperea5@gmail.com>
2024-05-15 13:49:10 -0300

    Add(-):Lazy Loading imports con CharPagination y CharSpecific

--- src/app/(with-nav-layout)/Personajes/[id]/page.jsx
+++ src/app/(with-nav-layout)/Personajes/[id]/page.jsx
@@ -1,5 +1,27 @@
 // En este componente se encontrará el componente principal de la ruta para Personaje especifica. Aquí se estará llamando a la API
-import CharSpecific from './CharSpecific'
+// import CharSpecific from './CharSpecific'
+import Image from 'next/image'
+
+// Aplicando Lazy Loading
+import dynamic from 'next/dynamic'
+const DynamicCharSpecific = dynamic(() => import('./CharSpecific'), {
+  loading: () => {
+    return (
+      <div className='w-full  flex flex-col items-center'>
+        <Image
+          src='/swlogo.svg'
+          alt='Loading Logo'
+          width={170}
+          height={170}
+          priority
+        />
+        <p>LOADING...</p>
+      </div>
+
+    )
+  }
+
+})
 
 const fetchOneChar = (id) => {
   return fetch(`https://swapi.dev/api/people/${id}`)
@@ -13,7 +35,7 @@ export default async function PersonajesSpecificPage ({ params }) {
   return (
     <section className='h-[800px] flex flex-col justify-center'>
 
-      <CharSpecific char={char} />
+      <DynamicCharSpecific char={char} />
     </section>
   )
 }
--- src/app/(with-nav-layout)/Personajes/page.jsx
+++ src/app/(with-nav-layout)/Personajes/page.jsx
@@ -1,8 +1,20 @@
 // La forma de navegar entre paginas de personajes, es con la url next y previous
 
-import CharPagination from './CharPagination'
+// import CharPagination from './CharPagination'
 import NavPagination from './NavPagination'
 import { fetchChar } from './filterLogic'
+// import Image from 'next/image'
+
+// Aplicando Lazy Loading para los componentes mas pesados //Basado en https://nextjs.org/docs/pages/building-your-application/optimizing/lazy-loading
+import dynamic from 'next/dynamic'
+const DynamicCharPagination = dynamic(() => import('./CharPagination'), {
+  loading: () => {
+    return (
+      <p>...CARGANDOOOOOO PAGINACIOOOOON</p>
+    )
+  }
+
+})
 
 export default async function Personajes (context) {
   const page = context.searchParams.page
@@ -19,7 +31,8 @@ export default async function Personajes (context) {
     <section>
       {/* A NavPagination le pasamos la pagina actual, la siguiente y la anterior. Mientras que CharPagination recibe los datos del fetch hecho. Cada vez que cambiamos de pagina, hace otra vez la llamada a la API  */}
       <NavPagination page={context.searchParams.page} prev={charsData.previous} next={charsData.next} />
-      <CharPagination chars={chars} />
+      <DynamicCharPagination chars={chars} />
+      {/* <CharPagination chars={chars} /> */}
     </section>
   )
 }
