189ad9e206e5b700fe9efec25da57da2d78a4746
Agustin <agustinperea5@gmail.com>
2024-05-15 15:14:04 -0300

    Drop(-):En Personajes y FilmSpecific cambie el Dynamic por el Suspense

--- src/app/(with-nav-layout)/Films/page.jsx
+++ src/app/(with-nav-layout)/Films/page.jsx
@@ -1,9 +1,16 @@
-import FilmsList from './filmsList'
+// import FilmsList from './filmsList'
+
+// Lazy Loading
+import Loader from '@/app/Loader'
+import dynamic from 'next/dynamic'
+const DynamicFilmList = dynamic(() => import('./FilmsList'), {
+  loading: () => <Loader />
+})
 
 export default async function Films ({ params }) {
   return (
     <section>
-      <FilmsList />
+      <DynamicFilmList />
     </section>
   )
 }
--- src/app/(with-nav-layout)/Personajes/page.jsx
+++ src/app/(with-nav-layout)/Personajes/page.jsx
@@ -1,15 +1,17 @@
 // La forma de navegar entre paginas de personajes, es con la url next y previous
 
-// import CharPagination from './CharPagination'
+import CharPagination from './CharPagination'
 import NavPagination from './NavPagination'
 import { fetchChar } from './filterLogic'
 
 // Aplicando Lazy Loading para los componentes mas pesados //Basado en https://nextjs.org/docs/pages/building-your-application/optimizing/lazy-loading
 import Loader from '@/app/Loader'
-import dynamic from 'next/dynamic'
-const DynamicCharPagination = dynamic(() => import('./CharPagination'), {
-  loading: () => <Loader />
-})
+// import dynamic from 'next/dynamic'
+// const DynamicCharPagination = dynamic(() => import('./CharPagination'), {
+//   loading: () => <Loader />
+// })
+// Para Lazy Loading
+import { Suspense } from 'react'
 
 export default async function Personajes (context) {
   const page = context.searchParams.page
@@ -23,11 +25,11 @@ export default async function Personajes (context) {
   // console.log({ context })
 
   return (
-    <section>
+    <Suspense fallback={<Loader />}>
       {/* A NavPagination le pasamos la pagina actual, la siguiente y la anterior. Mientras que CharPagination recibe los datos del fetch hecho. Cada vez que cambiamos de pagina, hace otra vez la llamada a la API  */}
       <NavPagination page={context.searchParams.page} prev={charsData.previous} next={charsData.next} />
-      <DynamicCharPagination chars={chars} />
+      <CharPagination chars={chars} />
       {/* <CharPagination chars={chars} /> */}
-    </section>
+    </Suspense>
   )
 }
